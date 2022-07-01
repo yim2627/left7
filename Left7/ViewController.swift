b323be99b65f95de014496141a18a6db0ecd3098
Jiseong <yim2627@gmail.com>
2022-07-01 15:56:14 +0900

    feat: init Project file

---
+++ Left7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Left7
+//
+//  Created by 임지성 on 2022/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
