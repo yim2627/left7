5c7d0e576f9455774b14e7c124aacbdbd23ec301
Jiseong <yim2627@gmail.com>
2022-07-01 18:47:28 +0900

    refactor: Response Model 수정 #2

--- Left7/Data/ResponseModel/YogiResponse.swift
+++ Left7/Data/ResponseModel/YogiResponse.swift
@@ -10,11 +10,46 @@ import Foundation
 struct YogiResponse: Decodable {
     let statusMsg: String
     let statusCode: Int
-    let productData: [ProductDataResponse]
+    let productData: ProductDataResponse
     
     enum CodingKeys: String, CodingKey {
         case statusMsg = "msg"
         case statusCode = "code"
         case productData = "data"
+    }
+}
+
+extension YogiResponse {
+    struct ProductDataResponse: Decodable {
+        let productTotalCount: Int
+        let product: [ProductResponse]
+        
+        enum CodingKeys: String, CodingKey {
+            case productTotalCount = "totalCount"
+            case product
+        }
+    }
+}
+
+extension YogiResponse.ProductDataResponse {
+    struct ProductResponse: Decodable {
+        let id: Int
+        let name: String
+        let thumbnailPath: String
+        let description: ProductDescriptionResponse
+        let rate: Double
+        
+        enum CodingKeys: String, CodingKey {
+            case id, name, description, rate
+            case thumbnailPath = "thumbnail"
+        }
+    }
+}
+
+extension YogiResponse.ProductDataResponse.ProductResponse {
+    struct ProductDescriptionResponse: Decodable {
+        let imagePath: String
+        let subject: String
+        let price: Int
     }
 }
--- Left7/ViewController.swift
+++ Left7/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        let end = EndPoint(urlInformation: .pagination(jsonId: 1))
+        let net = HttpNetwork()
+        net.fetch(endPoint: end)
+            .subscribe(onNext: { data in
+                let jsonDecoder = JSONDecoder()
+                let decodedData = try? jsonDecoder.decode(YogiResponse.self, from: data)
+                
+                print(decodedData)
+            })
     }
-
-
 }
 
