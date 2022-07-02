27292da2a917f46e3984678ffbfd7242b8cf9e33
Jiseong <yim2627@gmail.com>
2022-07-03 00:50:05 +0900

    feat: HomeUsecase 구현 및 Repository 의존성 주입 #11

---
+++ Left7/Domain/Usecase/YogiHomeUsecase.swift
@@ -0,0 +1,21 @@
+//
+//  YogiHomeUsecase.swift
+//  Left7
+//
+//  Created by 임지성 on 2022/07/03.
+//
+
+import Foundation
+import RxSwift
+
+final class YogiHomeUsecase {
+    private let productRepository: NetworkRepository
+    
+    init(productRepository: NetworkRepository) {
+        self.productRepository = productRepository
+    }
+    
+    func fetchProducts(page: Int) -> Observable<[Product]> {
+        productRepository.fetchYogiProducts(page: page)
+    }
+}
