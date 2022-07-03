40f5266e57feed20b0a580e18e10ec0573f9204a
Jiseong <yim2627@gmail.com>
2022-07-03 19:55:36 +0900

    feat: YogiDetailView 내부 컴포넌트 및 레이아웃 구현 #15

--- Left7/Presentation/Detail/YogiDetailViewController.swift
+++ Left7/Presentation/Detail/YogiDetailViewController.swift
@@ -10,24 +10,85 @@ import UIKit
 import SnapKit
 
 final class YogiDetailViewController: UIViewController {
+    private let productDetailScrollView = UIScrollView(frame: .zero)
+    
+    private let productDetailStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.backgroundColor = .white
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+    
     private let productImageView: YogiProductImageView = {
         let imageView = YogiProductImageView(frame: .zero)
         imageView.setImage(with: "https://gccompany.co.kr/App/image/img_1.jpg")
-        imageView.clipsToBounds = false
+        imageView.clipsToBounds = false 
         imageView.setFavoriteState(state: true)
         return imageView
     }()
     
+    private let productNameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.text = "호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자"
+        label.textColor = .black
+        return label
+    }()
+    
+    private let rateStackView = YogiRateStackView(frame: .zero)
+    
+    private let productPriceLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.textAlignment = .right
+        label.text = "30000원"
+        label.textColor = .black
+        return label
+    }()
+    
+    private let productSubjectLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .preferredFont(forTextStyle: .body)
+        label.text = "올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때"
+        label.textColor = .black
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.addSubview(productImageView)
-        
-        productImageView.snp.makeConstraints {
-            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
-            $0.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
-            $0.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
-            $0.height.equalTo(240)
+        configureYogiProductDetailView()
+    }
+    
+    private func configureYogiProductDetailView() {
+        configureProductDetailScrollView()
+        configureProductDetailStackView()
+    }
+    
+    private func configureProductDetailScrollView() {
+        self.view.addSubview(productDetailScrollView)
+        productDetailScrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
+    }
+    
+    private func configureProductDetailStackView() {
+        self.productDetailScrollView.addSubview(productDetailStackView)
+        productDetailStackView.snp.makeConstraints {
+            $0.edges.equalTo(productDetailScrollView.contentLayoutGuide)
+            $0.width.equalTo(productDetailScrollView.frameLayoutGuide)
+        }
+        
+        self.productDetailStackView.addArrangedSubview(productImageView)
+        productImageView.snp.makeConstraints {
+            $0.height.equalTo(300)
+        }
+        
+        self.productDetailStackView.addArrangedSubview(productNameLabel)
+        self.productDetailStackView.addArrangedSubview(rateStackView)
+        self.productDetailStackView.addArrangedSubview(productPriceLabel)
+        self.productDetailStackView.addArrangedSubview(productSubjectLabel)
     }
 }
