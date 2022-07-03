78afd2cbaa762bc7b18f7ca41555244604222f2f
Jiseong <yim2627@gmail.com>
2022-07-03 23:51:23 +0900

    feat: DetailVC 진입시 선택된 product 바인딩 구현 #17

--- Left7/Presentation/Detail/YogiDetailViewController.swift
+++ Left7/Presentation/Detail/YogiDetailViewController.swift
@@ -33,7 +33,6 @@ final class YogiDetailViewController: UIViewController, View {
     
     private let productImageView: UIImageView = {
         let imageView = UIImageView(frame: .zero)
-        imageView.setImage(with: "https://gccompany.co.kr/App/image/img_1.jpg")
         return imageView
     }()
     
@@ -41,18 +40,16 @@ final class YogiDetailViewController: UIViewController, View {
         let label = UILabel()
         label.numberOfLines = 0
         label.font = .preferredFont(forTextStyle: .title1)
-        label.text = "호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자호텔야자"
         label.textColor = .black
         return label
     }()
     
-    private let rateStackView = YogiRateStackView(frame: .zero)
+    private let productRateStackView = YogiRateStackView(frame: .zero)
     
     private let productPriceLabel: UILabel = {
         let label = UILabel()
         label.font = .preferredFont(forTextStyle: .headline)
         label.textAlignment = .right
-        label.text = "30000원"
         label.textColor = .black
         return label
     }()
@@ -61,7 +58,6 @@ final class YogiDetailViewController: UIViewController, View {
         let label = UILabel()
         label.numberOfLines = 0
         label.font = .preferredFont(forTextStyle: .body)
-        label.text = "올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때올여름혼자어때"
         label.textColor = .black
         return label
     }()
@@ -81,13 +77,19 @@ final class YogiDetailViewController: UIViewController, View {
         super.viewDidLoad()
         configureYogiProductDetailView()
         configureNavigationBar()
-        self.reactor = YogiDetailViewReactor()
     }
     
     func bind(reactor: YogiDetailViewReactor) {
         favoriteButton.rx.tap
             .map { _ in Reactor.Action.didTapFavoriteButton }
             .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .compactMap { $0.product }
+            .subscribe(onNext: { [weak self] in
+                self?.setData(product: $0)
+            })
             .disposed(by: disposeBag)
         
         reactor.state
@@ -132,7 +134,7 @@ final class YogiDetailViewController: UIViewController, View {
     
     private func configureProductDetailStackView() {
         self.productDetailStackView.addArrangedSubview(productNameLabel)
-        self.productDetailStackView.addArrangedSubview(rateStackView)
+        self.productDetailStackView.addArrangedSubview(productRateStackView)
         self.productDetailStackView.addArrangedSubview(productPriceLabel)
         
         self.productDetailStackView.addArrangedSubview(seperateLineView)
@@ -141,6 +143,15 @@ final class YogiDetailViewController: UIViewController, View {
         }
         
         self.productDetailStackView.addArrangedSubview(productSubjectLabel)
+    }
+    
+    private func setData(product: Product) {
+        productImageView.setImage(with: product.descriptionImagePath)
+        productNameLabel.text = product.name
+        productRateStackView.setRateValue(rate: product.rate)
+        productPriceLabel.text = "\(product.price)원"
+        productSubjectLabel.text = product.descriptionSubject
+        setFavoriteState(state: product.isFavorite)
     }
     
     private func setFavoriteState(state: Bool) {
