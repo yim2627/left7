4f08b0079442a3abe30b0e829187fd53d83c9368
Jiseong <yim2627@gmail.com>
2022-07-03 14:51:18 +0900

    feat: HomeViewReactor 액션 및 상태 바인딩 구현 #13

--- Left7/Presentation/Home/Reactor/YogiHomeViewReactor.swift
+++ Left7/Presentation/Home/Reactor/YogiHomeViewReactor.swift
@@ -13,25 +13,73 @@ import RxCocoa
 import ReactorKit
 
 final class YogiHomeViewReactor: Reactor {
+    private let useCase = YogiHomeUsecase()
     var initialState: State = State()
     
     enum Action {
-        
+        case fetchProducts
+        case loadNextPage
     }
     
-    enum Mutate {
-        
+    enum Mutation {
+        case setProducts([Product])
+        case appendProducts([Product], page: Int)
+        case setLoadingNextPage(Bool)
     }
     
     struct State {
-        
+        var products: [Product] = []
+        var page: Int = 1
+        var isLoadingNextPage: Bool = false
     }
     
-    func reduce(state: State, mutation: Action) -> State {
-        return state
+    func reduce(state: State, mutation: Mutation) -> State {
+        switch mutation {
+        case let .setProducts(products):
+            var newState = state
+            newState.products = products
+            return newState
+        case let .appendProducts(products, page: page):
+            var newState = state
+            newState.products = products
+            newState.page = page
+            return newState
+        case let .setLoadingNextPage(isLoadingNextPage):
+            var newState = state
+            newState.isLoadingNextPage = isLoadingNextPage
+            return newState
+        }
     }
     
-    func mutate(action: Action) -> Observable<Action> {
-        return .empty()
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .fetchProducts:
+            return useCase.fetchProducts(page: self.currentState.page)
+                .take(until: self.action.filter(Action.isUpdate))
+                .map { Mutation.setProducts($0) }
+            
+        case .loadNextPage:
+            guard !self.currentState.isLoadingNextPage else { return .empty() }
+            
+            return Observable.concat([
+                Observable.just(Mutation.setLoadingNextPage(true)),
+                
+                useCase.fetchProducts(page: self.currentState.page)
+                    .take(until: self.action.filter(Action.isUpdate))
+                    .map { Mutation.appendProducts($0, page: self.currentState.page + 1) },
+                
+                Observable.just(Mutation.setLoadingNextPage(false))
+            ])
+        }
+    }
+}
+
+extension YogiHomeViewReactor.Action {
+    static func isUpdate(_ action: YogiHomeViewReactor.Action) -> Bool {
+        if case .fetchProducts = action {
+            return true
+        } else {
+            return false
+        }
     }
 }
