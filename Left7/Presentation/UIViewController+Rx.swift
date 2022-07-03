6ab5c6d39699c5e79028c795814588a0406c19ee
Jiseong <yim2627@gmail.com>
2022-07-03 14:54:25 +0900

    feat: UIViewControllr Rx Extension 타입 구현 #13
    - viewDidLoad Event 구현

---
+++ Left7/Presentation/UIViewController+Rx.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+Rx.swift
+//  Left7
+//
+//  Created by 임지성 on 2022/07/03.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift  
+
+extension Reactive where Base: UIViewController {
+    var viewDidLoad: ControlEvent<Void> {
+        let viewDidLoadEvent = self.methodInvoked(#selector(base.viewDidLoad)).map { _ in }
+        return ControlEvent(events: viewDidLoadEvent)
+    }
+}
