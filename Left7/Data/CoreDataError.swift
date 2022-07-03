4c6350b82a15f0ad61fc328378fe45b84168ebdd
Jiseong <yim2627@gmail.com>
2022-07-04 02:21:08 +0900

    feat: CoreDataError 구현 #20

---
+++ Left7/Data/CoreDataError.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataError.swift
+//  Left7
+//
+//  Created by 임지성 on 2022/07/04.
+//
+
+import Foundation
+
+enum CoreDataError: String, LocalizedError {
+    case FetchFail = "ERROR: CoreData Fetch Fail"
+    case SaveFail = "ERROR: CoreData Save Fail"
+    
+    var errorDescription: String {
+        return self.rawValue
+    }
+}
---
+++ Left7/Data/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  Left7
+//
+//  Created by 임지성 on 2022/07/04.
+//
+
+import Foundation
