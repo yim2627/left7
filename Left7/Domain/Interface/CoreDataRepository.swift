833a7a88d29c8ca08ef893e9fc8300ea65a90990
Jiseong <yim2627@gmail.com>
2022-07-04 13:15:44 +0900

    feat: CoreDataRepository 추상 프로토콜 구현 #22

---
+++ Left7/Domain/Interface/CoreDataRepository.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataRepository.swift
+//  Left7
+//
+//  Created by 임지성 on 2022/07/04.
+//
+
+import Foundation
+import RxSwift
+
+protocol CoreDataRepository {
+    func fetchFavoriteProduct() -> Observable<[Product]>
+    func saveFavoriteProduct(_ product: Product)
+    func deleteFavoriteProduct(_ product: Product)
+}
