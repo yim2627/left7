40898de183b3ff75af7fa2a86449b352fcb9fed9
Jiseong <yim2627@gmail.com>
2022-07-04 17:52:41 +0900

    feat: YogiFavoriteCollectionViewCell 생성 및 폴더 트리 변경 #29

---
+++ Left7/Presentation/Favorite/View/Cell/YogiFavoriteCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  YogiFavoriteCollectionViewCell.swift
+//  Left7
+//
+//  Created by 임지성 on 2022/07/04.
+//
+
+import UIKit
+
+class YogiFavoriteCollectionViewCell: UICollectionViewCell {
+    
+}
