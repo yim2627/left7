4ed7dcfd511859cdb6bde85efbc80e136e1ff51b
Jiseong <yim2627@gmail.com>
2022-07-04 17:51:24 +0900

    feat: YogiFavoriteViewController 생성 및 컬렉션뷰 구현 #29

---
+++ Left7/Presentation/Favorite/YogiFavoriteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  YogiFavoriteViewController.swift
+//  Left7
+//
+//  Created by 임지성 on 2022/07/04.
+//
+
+import UIKit
+
+class YogiFavoriteViewController: UIViewController {
+    private var yogiFavoriteCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
