4ce6d429091ac3f1beb7420bed6ec05d14a07d34
Jiseong <yim2627@gmail.com>
2022-07-04 15:49:23 +0900

    feat: HomeUsecase CoreDataRepository 주입 #26

--- Left7/Domain/Usecase/YogiHomeUsecase.swift
+++ Left7/Domain/Usecase/YogiHomeUsecase.swift
@@ -10,9 +10,14 @@ import RxSwift
 
 final class YogiHomeUsecase {
     private let productRepository: NetworkRepository
+    private let favoriteProductRepository: CoreDataRepository
     
-    init(productRepository: NetworkRepository = YogiProductRepository()) {
+    init(
+        productRepository: NetworkRepository = YogiProductRepository(),
+        favoriteProductRepository: CoreDataRepository = YogiFavoriteProductRepository()
+    ) {
         self.productRepository = productRepository
+        self.favoriteProductRepository = favoriteProductRepository
     }
     
     func fetchProducts(page: Int) -> Observable<[Product]> {
