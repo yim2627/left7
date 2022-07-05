3db5d38448a65de618e8a04380d3529b6882d9dd
Jiseong <yim2627@gmail.com>
2022-07-05 21:47:48 +0900

    refactor: YogiFavoriteUsecase delete 메서드 네이밍 및 로직 변경 #31

--- Left7/Domain/Usecase/YogiFavoriteUsecase.swift
+++ Left7/Domain/Usecase/YogiFavoriteUsecase.swift
@@ -20,15 +20,7 @@ final class YogiFavoriteUsecase {
         favoriteProductRepository.fetchFavoriteProduct()
     }
     
-    func updateFavoriteProduct(_ product: Product?) {
-        guard let product = product else {
-            return
-        }
-        
-        if product.isFavorite == true {
-            favoriteProductRepository.saveFavoriteProduct(product)
-        } else {
-            favoriteProductRepository.deleteFavoriteProduct(product)
-        }
+    func deleteFavoriteProduct(_ product: Product) {
+        favoriteProductRepository.deleteFavoriteProduct(product)
     }
 }
