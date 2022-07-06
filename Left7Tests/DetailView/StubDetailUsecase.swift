7977271f65dbdc76b75d9ecf174d777b126acb15
Jiseong <yim2627@gmail.com>
2022-07-06 21:04:48 +0900

    test: DetailViewReactor Test 위한 DetailUsecase stub 구현 #49

---
+++ Left7Tests/DetailView/StubDetailUsecase.swift
@@ -0,0 +1,15 @@
+//
+//  StubDetailUsecase.swift
+//  Left7Tests
+//
+//  Created by 임지성 on 2022/07/06.
+//
+
+import Foundation
+@testable import Left7
+
+final class StubDetailUsecase: YogiDetailUsecaseType {
+    func updateFavoriteProduct(_ product: Product?) {
+        return
+    }
+}
