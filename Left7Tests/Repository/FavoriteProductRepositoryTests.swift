c3a4b5fc8018c0f1a3ded4a8e369541e8af09ce7
Jiseong <yim2627@gmail.com>
2022-07-07 13:25:25 +0900

    test: FavoriteProductRepositoryTests 타입 생성 #49

---
+++ Left7Tests/Repository/FavoriteProductRepositoryTests.swift
@@ -0,0 +1,11 @@
+//
+//  FavoriteProductRepositoryTests.swift
+//  Left7Tests
+//
+//  Created by 임지성 on 2022/07/07.
+//
+
+import XCTest
+@testable import Left7
+
+class FavoriteProductRepositoryTests: XCTestCase { }
